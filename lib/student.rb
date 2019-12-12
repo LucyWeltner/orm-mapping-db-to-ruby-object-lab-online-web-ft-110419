493582bddfcd15d8b4adee29c63b2a394b90b4f3
Lucy Weltner <lucyiweltner@gmail.com>
2019-12-12 00:07:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,6 +65,14 @@ class Student
   def self.first_X_students_in_grade_10(x)
     students = self.all.select{|student| student.grade == 10}[0..x-1]
   end
+  
+  def self.first_student_in_grade_10 
+    sql = "SELECT * FROM students WHERE grade = 10 ORDER BY id LIMIT 1"
+    student_info = DB[:conn].execute(sql)
+    p student_info
+    first_student = self.new_from_db(student_info)
+    first_student
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
