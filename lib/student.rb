27c892c125790e78d7f6e11db4b917e83fb438b3
Lucy Weltner <lucyiweltner@gmail.com>
2019-12-12 00:03:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,7 @@ class Student
   end
   
   def self.all_students_in_grade_9
-    grade_9_students = self.all.select{|student| student.grade = 9}
+    grade_9_students = self.all.select{|student| student.grade == 9}
   end 
   
   def self.students_below_12th_grade
